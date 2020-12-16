6348033eb95f83e2333125e5943608aa19d3de3b
Zachary Charles Leighton <zleight1@gmail.com>
2020-12-16 15:15:41 +0200

    WIP

---
+++ new-plugin/src/index.js
@@ -0,0 +1,4 @@
+// import lodash from 'lodash';
+
+console.log('Hello World from your main file!');
+console.log('Hello World from your main file!');
---
+++ new-plugin/webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+
+
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+/*
+ * We've enabled TerserPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/terser-webpack-plugin
+ *
+ */
+
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ImportMapPlugin = require('./plugin');
+const HelloPlugin = require('./hello-plugin');
+
+
+
+
+module.exports = {
+  mode: 'development',
+  plugins: [
+    new webpack.ProgressPlugin(),
+     new CleanWebpackPlugin(), 
+     new ImportMapPlugin(),
+    //  new HelloPlugin(),
+    ],
+
+  output: {
+    filename: '[name].[contenthash].bundle.js',
+    chunkFilename: '[name].[contenthash].chunk.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      include: [path.resolve(__dirname, 'src')],
+      loader: 'babel-loader'
+    }]
+  },
+
+  optimization: {
+    minimizer: [new TerserPlugin()],
+
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
+
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: false
+    }
+  }
+}
